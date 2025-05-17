130bf8ff3da2cb2a00c9e03b2a5c9744dbe2038e
RyamL1221 <ryaml1221@gmail.com>
2025-05-17 15:23:42 -0400

    added macd and rsi

--- src/util/polygon.py
+++ src/util/polygon.py
@@ -1,5 +1,7 @@
 BASE_API_URL = "https://api.polygon.io"
 ENDPOINTS = {
     "GET_TICKER": "/v3/reference/tickers",
-    "MACD": "/v1/indicators/macd"
+    "MACD": "/v1/indicators/macd",
+    "RSI": "/v1/indicators/rsi"
+
 }
