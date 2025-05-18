b99a8161a1112c0d4d22187ef7290df4032f9534
RyamL1221 <ryaml1221@gmail.com>
2025-05-18 08:59:16 -0400

    added company profile

--- src/routes/get_stock_ticker/get_stock_ticker.py
+++ src/routes/get_stock_ticker/get_stock_ticker.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, make_response, jsonify
-from src.util.polygon import BASE_API_URL, ENDPOINTS
+from src.util.api_urls import POLYGON_BASE_API_URL, POLYGON_ENDPOINTS, FINNHUB_BASE_API_URL, FINNHUB_ENDPOINTS
 import os, requests
 
 get_stock_ticker_bp = Blueprint("get_stock_ticker", __name__)
@@ -7,11 +7,20 @@ get_stock_ticker_bp = Blueprint("get_stock_ticker", __name__)
 @get_stock_ticker_bp.route('/get-stock-ticker/<string:stock_ticker>', methods=['GET'])
 def get_stock_ticker(stock_ticker):
     stock_ticker = stock_ticker.upper()
-    api_key = os.getenv("POLYGON_API_KEY")
-    headers = {"Authorization": f"Bearer {api_key}"}
 
-    macd_url = f"{BASE_API_URL}{ENDPOINTS['MACD']}/{stock_ticker}"
-    rsi_url  = f"{BASE_API_URL}{ENDPOINTS['RSI']}/{stock_ticker}"
+    # Polygon API setup
+    polygon_api_key = os.getenv("POLYGON_API_KEY")
+    headers = {"Authorization": f"Bearer {polygon_api_key}"}
+    macd_url = f"{POLYGON_BASE_API_URL}{POLYGON_ENDPOINTS['MACD']}/{stock_ticker}"
+    rsi_url  = f"{POLYGON_BASE_API_URL}{POLYGON_ENDPOINTS['RSI']}/{stock_ticker}"
+
+    # Finhubb API setup
+    finnhub_api_key = os.getenv("FINNHUB_API_KEY")
+    finnhub_url = (
+        f"{FINNHUB_BASE_API_URL}{FINNHUB_ENDPOINTS['COMPANY_PROFILE2']}"
+        f"?symbol={stock_ticker}"
+    )
+    finnhub_headers = {"X-Finnhub-Token": finnhub_api_key}
 
     try:
         macd_resp = requests.get(macd_url, headers=headers, timeout=5)
@@ -21,6 +30,14 @@ def get_stock_ticker(stock_ticker):
         rsi_resp = requests.get(rsi_url, headers=headers, timeout=5)
         rsi_resp.raise_for_status()
         rsi_data = rsi_resp.json()
+
+        
+        profile_resp = requests.get(
+            finnhub_url,
+            headers=finnhub_headers
+        )
+        profile_resp.raise_for_status()
+        profile = profile_resp.json()
 
     except requests.exceptions.HTTPError as http_err:
         msg = f"Polygon API returned {http_err.response.status_code}: {http_err}"
@@ -39,7 +56,11 @@ def get_stock_ticker(stock_ticker):
     # Above 80 is overbought, below 30 is oversold for RSI
     # Unix Msec Time for timestamp
     return jsonify({
-        "ticker": stock_ticker,
-        "macd": macd_values,
-        "rsi":  rsi_values
+        "ticker":           stock_ticker,
+        "macd":             macd_values,
+        "rsi":              rsi_values,
+        "country":          profile.get("country"),
+        "name":             profile.get("name"),
+        "shareOutstanding": profile.get("shareOutstanding"),
+        "weburl":           profile.get("weburl"),
     }), 200
--- src/util/polygon.py
+++
@@ -1,7 +0,0 @@
-BASE_API_URL = "https://api.polygon.io"
-ENDPOINTS = {
-    "GET_TICKER": "/v3/reference/tickers",
-    "MACD": "/v1/indicators/macd",
-    "RSI": "/v1/indicators/rsi"
-
-}
